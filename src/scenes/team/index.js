2f0b6ada0dc345f933ffb8fd73ac97632d8ff172
Masquerade-7991 <drklrd077@gmail.com>
2025-09-10 22:42:12 +0530

    minor changes

---
+++ src/scenes/team/index.js
@@ -0,0 +1,28 @@
+import { Box, Typography, useTheme } from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
+import { tokens } from '../../theme';
+import { mockDataTeam } from '../../data/mockData';
+import AdminPanelSettingsOutlinedIcon from '@mui/icons-material/AdminPanelSettingsOutlined';
+import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
+import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined";
+import Header from '../../components/Header';
+
+const Team = () => { 
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  const columns = [{field: "id", headerName: "ID"}, {field: "name", headerName:"NAME"}, ]
+
+  return (
+    <Box>
+      <Header title='TEAM' subtitle="Managing the team Members" />
+        <Box>
+          <DataGrid rows={mockDataTeam} columns={columns}>
+
+          </DataGrid>
+        </Box>
+    </Box>
+  )
+}
+
+export default Team;
